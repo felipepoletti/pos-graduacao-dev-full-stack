283be5db35b0904454178bbbbaa912316c962025
= <=>
2023-07-19 19:12:55 -0300

    Learning about iterator and comparator with Java

---
+++ IteratorAndComparator/src/pos/IteratorAndComparator/Main.java
@@ -0,0 +1,25 @@
+package pos.IteratorAndComparator;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
+	public static void main(String[] args) {
+		ArrayList<Integer> numeros = new ArrayList<>();
+		numeros.add(1);
+		numeros.add(2);
+		numeros.add(3);
+		
+		System.out.println("ArrayList: " + numeros);
+		
+		// Criando uma instância de Iterator
+		Iterator<Integer> it = numeros.iterator();
+		
+		int numero = it.next();
+		
+		System.out.println("Elemento: " + numero);
+		
+		while(it.hasNext()) {
+			it.forEachRemaining((value) -> System.out.print(value + ", "));
+		}
+	}
+}
